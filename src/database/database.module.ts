95efc3673d3911e5ba6bd80a13db5c78ea63ddff
irfanardians <96757328+irfanardians@users.noreply.github.com>
2022-02-04 22:13:13 +0700

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -12,6 +12,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           username: configService.get('POSTGRES_USER'),
           password: configService.get('POSTGRES_PASSWORD'),
           database: configService.get('POSTGRES_DB'),
+          ssl: true,
+                    extra: {
+                        ssl: {
+                          rejectUnauthorized: false
+                        },
+                      },
           entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
           ],
